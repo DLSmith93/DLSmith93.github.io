3d59551ecd651406425abc126fb0e1e6564d0625
DLSmith93 <94343037+DLSmith93@users.noreply.github.com>
2023-09-17 14:48:08 -0700

    Delete function directory

--- function/index.js
+++
@@ -1,8 +0,0 @@
-function irish(){
-    
-    var string = "Kiss me, I'm Irish!";
-    var result = string.fontcolor("red");
-    confirm(string);
-    document.getElementById("Irish").innerHTML = result;
-  
-}
